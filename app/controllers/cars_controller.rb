5befeadec32879f1c45b7efebc81465f559ca29b
Gauthier Lepour <gauthier.lepour@gmail.com>
2024-02-27 15:25:01 +0100

    Add dotenv-rails

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,23 @@
 class CarsController < ApplicationController
+  # before_action :authenticate_user!, :except => [:index, :show]
   def show
     @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    @car.save
+    redirect_to new_car_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :price_per_day, :address, :description, :km, :year, :seats, :carburant, :doors, :power)
   end
 end
