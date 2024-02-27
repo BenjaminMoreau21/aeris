5ad61c0ce644a2dc76b0af8d21fb3a7fc0538c8e
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-02-27 18:12:03 +0100

    Back done need front

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,6 +6,7 @@ class CarsController < ApplicationController
 
   def show
     @car = Car.find(params[:id])
+    @review = Review.new
   end
 
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_144727) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_162727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_144727) do
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reviews_on_car_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -77,4 +88,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_144727) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cars", "users"
+  add_foreign_key "reviews", "cars"
+  add_foreign_key "reviews", "users"
 end
