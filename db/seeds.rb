cecf936f77c361a70ad14f69599931855f524b74
Gauthier Lepour <gauthier.lepour@gmail.com>
2024-03-01 15:45:20 +0100

    seeds porsche lambo

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,61 +18,68 @@ puts "Users created"
 
 puts "Creating cars..."
 
-file = URI.open("https://www.flat6mag.com/medias/images/interieur-porsche-911-st-flat-6-mag.png")
-file2 = URI.open("https://cdn.motor1.com/images/mgl/6ZAvXk/s1/lamborghini-invencible.jpg")
-car = Car.new(brand: 'Porscheeeee', model: 'Taycan', price_per_day: 234,
-  address: 'leWagon Street 50', km: '20000', year: '2016', seats: '4',
-  carburant: 'Diesel', doors: 5, power: 300, user: user2, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "porsche.png", content_type: "image/png")
-car.photos.attach(io: file2, filename: "porsche44.png", content_type: "image/png")
+file = URI.open("https://robbreport.com/wp-content/uploads/2021/07/1-15.jpg?w=1000")
+file2 = URI.open("https://www.tomhartleyjnr.com/wp-content/uploads/2019/05/OU14EPO-000_1024x768.jpg")
+file3 = URI.open("https://www.goodcarbadcar.net/wp-content/uploads/2014/03/Porsche-918-Spyder-scaled.jpeg")
+car = Car.new(brand: 'Porschette ', model: '918 spyder', price_per_day: 769,
+  address: 'Bollywood', km: '27000', year: '2016', seats: '2',
+  carburant: 'Diesel', doors: 5, power: 600, user: user2, description: "La Porsche 918 a été élue « meilleure voiture du monde » aux Joiris Car Awards 2013.")
+car.photos.attach(io: file, filename: "porsche.png", content_type: "image/jpeg")
+car.photos.attach(io: file2, filename: "porsche44.png", content_type: "image/jpeg")
+car.photos.attach(io: file3, filename: "porsche44.png", content_type: "image/jpeg")
 car.save!
 
-file = URI.open("https://cdn.motor1.com/images/mgl/6ZAvXk/s1/lamborghini-invencible.jpg")
-car = Car.new(brand: 'Lamborghini', model: 'Aventador', price_per_day: 40,
-  address: 'leWagon Street 50', km: '20000', year: '2021', seats: '2',
-  carburant: 'Essence', doors: 5, power: 650, user: user1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "lambo.jpg", content_type: "image/jpg")
+
+file = URI.open("https://www.cnet.com/a/img/resize/0d5e427559463719f6f1c76427b0eca912f40013/hub/2022/03/24/9cc3b228-41e7-4f03-97ce-a94655cdead1/2022-porsche-911-turbo-s-006.jpg?auto=webp&width=1200")
+file2 = URI.open("https://i0.wp.com/asphalt9.info/wp-content/uploads/2019/06/Asphalt-9-Porsche-911-Targa-4S-5.jpeg?ssl=1")
+file3 = URI.open("https://hips.hearstapps.com/hmg-prod/images/2021-porsche-911-turbo-s-pdk-296-1608061360.jpg?crop=0.609xw:0.456xh;0.322xw,0.475xh&resize=1200:*")
+car = Car.new(brand: 'Porsche', model: 'turbo 911 ', price_per_day: 999,
+  address: 'Happy Street', km: '6000', year: '2014', seats: '2',
+  carburant: 'Diesel', doors: 2, power: 500, user: user2, description: "La Porsche 911 est une voiture de sport haut de gamme fabriquée par le constructeur automobile allemand Porsche. La première génération est commercialisée en 1964, intégralement conçue par la firme de Stuttgart. Soixante ans plus tard, ce modèle, emblématique de la marque, a conservé son esthétique générale. Il est toujours produit et commercialisé dans sa dernière version en date, la Porsche 911 type 992. L'architecture du moteur demeure inchangée, un 6-cylindres à plat (flat-six) disposé en porte-à-faux arrière, à l'exception des Porsche 911 GT1 et Porsche 991 RSR, voitures de course dotées d'un moteur central arrière.")
+car.photos.attach(io: file, filename: "porsche.png", content_type: "image/jpeg")
+car.photos.attach(io: file2, filename: "porsche44.png", content_type: "image/jpeg")
+car.photos.attach(io: file3, filename: "porsche44.png", content_type: "image/jpeg")
 car.save!
 
-file = URI.open("https://www.autoscout24.be/cms-content-assets/3ajyAoKA67PtPgPAiV7FHC-f8909352ff7e80aba11c29b9c3d1b672-Medium-2226-McLarenP1Bahrain-1100.jpg")
-car = Car.new(brand: 'McLaren', model: '720s', price_per_day: 342,
-  address: 'Wavre', km: '20000', year: '2021', seats: '2',
-  carburant: 'Diesel', doors: 5, power: 650, user: user1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "lambo.jpg", content_type: "image/jpg")
+file = URI.open("https://www.largus.fr/images/images/essai-porsche-911-992-targa-2020-6.jpg")
+file2 = URI.open("https://preview.free3d.com/img/2020/05/2324869464421565528/9w034fig.jpg")
+car = Car.new(brand: 'Porsche ', model: '911 targa 4s', price_per_day: 555,
+  address: 'Barbie Town', km: '27000', year: '2020', seats: '4',
+  carburant: 'Diesel', doors: 5, power: 700, user: user2, description: "La Porsche 918 a été élue « meilleure voiture du monde » aux Joiris Car Awards 2013.")
+car.photos.attach(io: file, filename: "porsche.png", content_type: "image/jpeg")
+car.photos.attach(io: file2, filename: "porsche44.png", content_type: "image/jpeg")
 car.save!
 
-file = URI.open("https://www.flat6mag.com/medias/images/porsche-taycan-baie-givree-arriere.jpg")
-car = Car.new(brand: 'Porsche', model: 'Taycan', price_per_day: 234,
-  address: 'Wavre', km: '2346', year: '2023', seats: '2',
-  carburant: 'Electric', doors: 5, power: 765, user: user3, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "porsche2.jpg", content_type: "image/jpg")
+
+file = URI.open("https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/facelift_2019/model_detail/aventador/s_roadster/s/s-3_M.jpg")
+file2 = URI.open("https://cdn.motor1.com/images/mgl/4JyZA/s1/lamborghini-aventador-lp-780-4-ultimae.webp")
+file3 = URI.open("https://www.alainclass.com/wp-content/uploads/inventory/cf529fd6-7aca-4aa1-abe4-eb4705378337/resized_IMG_4471.jpg")
+car = Car.new(brand: 'Lamborghini ', model: 'aventador ', price_per_day: 666,
+  address: 'Sir Pedro Street', km: '420', year: '1989', seats: '2',
+  carburant: 'Diesel', doors: 2, power: 420, user: user2, description: "Pour Automobili Lamborghini, l’esprit révolutionnaire est une marque de fabrique : qu’il s’agisse de design inspiré de l’aéronautique ou de technologies comme celle du moteur V12 ou de la fibre de carbone, repousser les limites communément admises fait partie de notre philosophie. L’Aventador a été conçue pour aller au-delà du concept de performance, faisant d’emblée figure de référence dans le domaine des supersportives et devançant les voitures du futur. Une famille de supercars déjà inscrite dans la légende.")
+car.photos.attach(io: file, filename: "lambo.png", content_type: "image/jpeg")
+car.photos.attach(io: file2, filename: "lambo44.png", content_type: "image/jpeg")
+car.photos.attach(io: file3, filename: "lambo44.png", content_type: "image/jpeg")
 car.save!
 
-file = URI.open("https://www.pngall.com/wp-content/uploads/2018/04/Luxury-Car-PNG-HD.png")
-car = Car.new(brand: 'Ferrari', model: 'La Ferrari', price_per_day: 354,
-  address: 'Paris', km: '554', year: '2006', seats: '2',
-  carburant: 'Essence', doors: 5, power: 766, user: user1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "lambo.jpg", content_type: "image/jpg")
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Lamborghini_Gallardo_3.jpg/1200px-Lamborghini_Gallardo_3.jpg")
+file2 = URI.open("https://collectingcars.imgix.net/016991/103Lamborghini-Gallardo-Superleggera-Maasdijk-Automotive-Collecting-Cars.jpg?w=1263&fit=fillmax&crop=edges&auto=format,compress&cs=srgb&q=85")
+file3 = URI.open("https://car-images.bauersecure.com/wp-images/11907/480x270/lambosuperlegdrive_1_560px.jpg")
+car = Car.new(brand: 'Lamborghini ', model: 'gallardo  ', price_per_day: 123,
+  address: 'Pan Town', km: '1200', year: '2024', seats: '2',
+  carburant: 'Diesel', doors: 2, power: 420, user: user2, description: "La Lamborghini Gallardo est une voiture de sport du constructeur automobile italien Lamborghini. Sa conception est dirigée par Audi, propriétaire de la marque depuis 1998. Très attendue lors de sa présentation au salon automobile de Genève 2003, elle est conçue pour concurrencer la Ferrari 360 Modena1? la marque ayant préalablement dévoilé son moteur V10 de 5 litres au Mondial de l'automobile de Paris 20022.  ")
+car.photos.attach(io: file, filename: "lambo.png", content_type: "image/jpeg")
+car.photos.attach(io: file2, filename: "lambo44.png", content_type: "image/jpeg")
+car.photos.attach(io: file3, filename: "lambo44.png", content_type: "image/jpeg")
 car.save!
 
-file = URI.open("https://www.flat6mag.com/medias/images/interieur-porsche-911-st-flat-6-mag.png")
-car = Car.new(brand: 'Mercedes', model: 'AMG A-35', price_per_day: 40,
-  address: 'leWagon Street 50', km: '20000', year: '2016', seats: '4',
-  carburant: 'Diesel', doors: 5, power: 300, user: user1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "porsche.png", content_type: "image/png")
-car.save!
 
-file = URI.open("https://www.flat6mag.com/medias/images/porsche-taycan-baie-givree-arriere.jpg")
-car = Car.new(brand: 'Porsche', model: 'Taycan', price_per_day: 234,
-  address: 'Wavre', km: '2346', year: '2023', seats: '2',
-  carburant: 'Electric', doors: 5, power: 765, user: user1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "porsche2.jpg", content_type: "image/jpg")
+file = URI.open("https://s1.cdn.autoevolution.com/images/news/gallery/is-the-classic-lamborghini-countach-now-a-10-million-affair_1.jpg")
+file2 = URI.open("https://simonfurlonger.co.uk/cars/enlarge/166783150431064.jpg")
+car = Car.new(brand: 'Lamborghini ', model: 'countach ', price_per_day: 222,
+  address: 'Bikini Bottom', km: '1340', year: '2002', seats: '2',
+  carburant: 'Diesel', doors: 2, power: 420, user: user2, description: "La Lamborghini Countach est une supercar GT à moteur V12 du constructeur automobile italien Lamborghini, produite à 2 042 exemplaires entre 1974 et 1990.")
+car.photos.attach(io: file, filename: "lambo.png", content_type: "image/jpeg")
+car.photos.attach(io: file2, filename: "lambo44.png", content_type: "image/jpeg")
 car.save!
-
-file = URI.open("https://purepng.com/public/uploads/large/purepng.com-mercedes-benz-s63-amg-white-carcarvehicletransportmercedes-benz-961524652051tpgtg.png")
-car = Car.new(brand: 'Mercedes', model: 'class S', price_per_day: 132,
-  address: 'Wavre', km: '2346', year: '2023', seats: '2',
-  carburant: 'Essence', doors: 5, power: 345, user: user1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-car.photos.attach(io: file, filename: "porsche2.jpg", content_type: "image/jpg")
-car.save!
-puts "Cars created"
